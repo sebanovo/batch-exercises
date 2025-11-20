67f9dc3425a8c857bb336a81e3b4982e2994691a
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-11-20 14:20:20 -0400

    add pipe

--- stdinout.bat
+++ stdinout.bat
@@ -1,4 +1,14 @@
 @echo off
+del append.txt
+del create.txt
 
 echo create > create.txt
-echo append >> append.txt
+
+echo b >> append.txt
+echo e >> append.txt
+echo a >> append.txt
+echo y >> append.txt
+echo o >> append.txt
+echo r >> append.txt
+
+type append.txt | sort /R >> ordenado.txt
